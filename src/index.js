4aee79ddb72d3fa4b5c1e2ef7296982d877e3277
Nagib Tharani <ntharani@gmail.com>
2017-11-20 17:10:40 -0800

    add boilerplate code for redux promise, devtools and reducer

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bulma/css/bulma.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose;
+
+const store = createStoreWithMiddleware(reducers,{},composeEnhancers)
  
+
+
 ReactDOM.render(
-<BrowserRouter>
-  <div>
-    <Route exact path="/" component={App} />
-  </div>
-</BrowserRouter>
+<Provider store={store}>
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </BrowserRouter>
+</Provider>
 , document.getElementById('root'));
 registerServiceWorker();
